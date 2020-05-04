b8467212fbe8dee701b95fc16e09288c2b041f2b
Oueslati-Waafa <63404321+Oueslati-Waafa@users.noreply.github.com>
2020-05-04 16:23:37 +0100

    Create CategoryDetail.cs

---
+++ Models/CategoryDetail.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ecommerce.Models
+{
+    public class CategoryDetail
+    {
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Le nom de la catégorie est obligatoire")]
+        [StringLength(100, ErrorMessage = "veuillez respecter le nombre des caractéres", MinimumLength = 3)]
+        public string CategoryName { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+    }
+
+    public class ProductDetail
+    {
+        public int ProductId { get; set; }
+        [Required(ErrorMessage = "Le nom du produit est obligatoire")]
+        [StringLength(100, ErrorMessage = "veuillez respecter le nombre des caractéres", MinimumLength = 3)]
+        public string ProductName { get; set; }
+        [Required]
+        [Range(1, 50)]
+        public Nullable<int> CategoryId { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+        [Required(ErrorMessage = "La description est obligatoire")]
+        public Nullable<System.DateTime> Description { get; set; }
+        public string ProductImage { get; set; }
+        public Nullable<bool> IsFeatured { get; set; }
+        [Required]
+        [Range(typeof(int), "1", "500", ErrorMessage = "Quantité invalide")]
+        public Nullable<int> Quantity { get; set; }
+        [Required]
+        [Range(typeof(decimal), "1", "200000", ErrorMessage = "Prix invalide")]
+        public Nullable<decimal> Price { get; set; }
+        public SelectList Categories { get; set; }
+    }
+}
